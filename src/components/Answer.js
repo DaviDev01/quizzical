c58bde47b834e7403d939fa2cac7035916d74f70
DaviDev01 <davimendesdafonseca@outlook.com>
2021-12-01 13:05:43 -0300

    dryer and quotes bug free
    made is dryer and fixed quotes bug

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,5 +1,13 @@
 import React from "react"
 
 export default function Answer(props) {
-    return <span className="q-and-a--a">{props.answer}</span>
+    return (
+        <button 
+            disabled={props.showCorrect}
+            style={props.getStyle(props.answerObj)} 
+            onClick={() => props.handleClick(props.answerObj.id, props.objId)} 
+            className="q-and-a--a" 
+            dangerouslySetInnerHTML={{__html: props.answerObj.answer }}
+        />
+    )
 }
--- src/components/QandA.js
+++ src/components/QandA.js
@@ -1,46 +1,25 @@
 import React, {useState} from "react"
-//import Answer from "./Answer"
+import Answer from "./Answer"
 
 export default function QandA(props) {
     const [randomNum] = useState(Math.random() - 0.5)
     const newAnswersArray = [...props.answers, props.correctAnswer ].sort(() => randomNum)
 
-    console.log(newAnswersArray)
+    const options = newAnswersArray.map( (answer) => (
+        <Answer 
+        showCorrect={props.showCorrect}
+        getStyle={props.getStyle}
+        answerObj={answer}
+        handleClick={props.handleClick}
+        objId={props.objId}
+        />        
+    ))
+
     return (
         <section className="q-and-a">
-            <h3 className="q-and-a--q">{props.question}</h3>
+            <h3 className="q-and-a--q" dangerouslySetInnerHTML={{__html:props.question }} />
             <div className="q-and-a--a_container">
-                <button 
-                    disabled={props.showCorrect}
-                    style={props.getStyle(newAnswersArray[0])} 
-                    onClick={() => props.handleClick(newAnswersArray[0].id, props.objId)} 
-                    className="q-and-a--a" 
-                >{newAnswersArray[0].answer}  
-                </button>
-
-                <button 
-                    disabled={props.showCorrect}
-                    style={props.getStyle(newAnswersArray[1])} 
-                    onClick={() => props.handleClick(newAnswersArray[1].id, props.objId)} 
-                    className="q-and-a--a"
-                >{newAnswersArray[1].answer}
-                </button>
-
-                <button 
-                    disabled={props.showCorrect}
-                    style={props.getStyle(newAnswersArray[2])} 
-                    onClick={() => props.handleClick(newAnswersArray[2].id, props.objId)} 
-                    className="q-and-a--a"
-                >{newAnswersArray[2].answer}
-                </button>
-
-                <button 
-                    disabled={props.showCorrect}
-                    style={props.getStyle(newAnswersArray[3])} 
-                    onClick={() => props.handleClick(newAnswersArray[3].id, props.objId)} 
-                    className="q-and-a--a"
-                >{newAnswersArray[3].answer}
-                </button>
+                {options}
             </div>
         </section> 
     ) 
